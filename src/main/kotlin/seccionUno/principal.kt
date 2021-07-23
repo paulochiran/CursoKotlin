03d6832482254f697a76db766a2e7807cc7e3790
Paulo Alexander Chirán Portillo <paulo.chiran@mercadolibre.com.co>
2021-07-23 14:57:27 -0500

    Priemer Commit
    Envio principal

---
+++ src/main/kotlin/seccionUno/principal.kt
@@ -0,0 +1,15 @@
+package seccionUno
+
+//comentario 
+fun main(){
+    /**
+     * Comentario 1 aSa
+     */
+    println("1 Hola mundo !!")
+
+
+    println("2 Hola mundo !!")
+
+
+    println("3 Hola mundo !!")
+}
