5c7e7c11892e53be42ca49a9ec2fb5dbf8ba029b
Paulo Alexander Chirán Portillo <paulo.chiran@mercadolibre.com.co>
2021-07-23 15:27:34 -0500

    cambios 2
    cambios 2.

--- src/main/kotlin/seccionUno/principal.kt
+++ src/main/kotlin/seccionUno/principal.kt
@@ -2,14 +2,22 @@ package seccionUno
 
 //comentario 
 fun main(){
+
     /**
      * Comentario 1 aSa
      */
+    
     println("1 Hola mundo !!")
 
+    /**
+     * Comentario 1 aSa
+     */
 
     println("2 Hola mundo !!")
 
+    /**
+     * Comentario 1 aSa
+     */
 
     println("3 Hola mundo !!")
 }
